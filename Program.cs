b41f2d366899d39957c491a1bcb037fd3e7787ef
ahmedkmal507 <149874204+ahmedkmal507@users.noreply.github.com>
2024-05-21 18:38:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Project;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbcontext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Data Source=MuhammedHani\\AKSQLEXPRESS;Initial Catalog=project;Integrated Security=True;Encrypt=True;Trust Server Certificate=True")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
